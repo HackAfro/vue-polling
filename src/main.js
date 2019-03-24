9df90a0bcdeb4fa5041edb60c2a5d7cc5edb776b
Richard Afro <richyafro@hotmail.com>
2019-03-24 13:09:20 +0100

    Add project plugins

--- src/main.js
+++ src/main.js
@@ -1,8 +1,23 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import VSocket from 'vue-socket.io';
+import '@progress/kendo-ui';
+import '@progress/kendo-theme-default/dist/';
 
-Vue.config.productionTip = false
+import {
+  ChartInstaller,
+} from '@progress/kendo-charts-vue-wrapper';
+import App from './App.vue';
+
+Vue.use(
+  new VSocket({
+    debug: true,
+    connection: 'http://localhost:4000',
+  })
+);
+
+Vue.use(ChartInstaller);
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount('#app');
