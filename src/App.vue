257bea413c315b83a2233900459d235cd73111be
Richard Afro <richyafro@hotmail.com>
2019-03-24 13:09:12 +0100

    Update app component

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,80 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header/>
+    <div class="body">
+      <Poll :options="options"/>
+      <PollsChart :options="options" v-if="voted"/>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from "./components/Header";
+import Poll from "./components/Poll";
+import PollsChart from "./components/Chart";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    HelloWorld
+    Header,
+    Poll,
+    PollsChart
+  },
+  data() {
+    return {
+      options: [
+        {
+          value: 0,
+          id: 1,
+          category: "Before 9:00pm"
+        },
+        {
+          value: 0,
+          id: 2,
+          category: "After 9:00pm before 10:00pm"
+        },
+        {
+          value: 0,
+          id: 3,
+          category: "Before 11:00pm"
+        },
+        {
+          value: 0,
+          id: 4,
+          category: "Early hours - 12:00am"
+        }
+      ],
+      voted: false
+    };
+  },
+  sockets: {
+    connect() {
+      console.log("connected");
+    },
+    voted(data) {
+      console.log(this.options);
+      this.options = this.options.map(option => {
+        if (data.id === option.id) {
+          option.value += 1;
+          return option;
+        }
+        return option;
+      });
+      this.voted = true;
+    }
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  width: 70%;
+  margin: auto;
   color: #2c3e50;
-  margin-top: 60px;
+  font-family: muli, sans-serif;
+  font-weight: 400;
+}
+.body {
+  display: flex;
 }
 </style>
